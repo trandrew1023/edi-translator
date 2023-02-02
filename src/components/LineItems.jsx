419861096573a97788352bd9464e83b1598d8f15
Michael Harlow <michaelharlow1@outlook.com>
2023-02-02 13:30:16 -0600

    Mui List component & Line item numbers

--- src/components/LineItems.jsx
+++ src/components/LineItems.jsx
@@ -1,5 +1,6 @@
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { Grid, IconButton, Typography } from '@mui/material';
+import { ListItem, List } from '@mui/material'; //TODO MAKE SURE TO CHANGE THIS IMPORT
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 import { React } from 'react';
@@ -38,17 +39,32 @@ function LineItems({ lineItems, lineItemErrors, setLineItems }) {
       <Grid item xs={12}>
         <Typography variant="h4">Line Items</Typography>
       </Grid>
-      {lineItems &&
-        Array.from(lineItems).map(([key, lineItem]) => (
-          <LineItem
-            key={key}
-            lineItem={lineItem}
-            lineItemError={lineItemErrors.get(key)}
-            mapID={key}
-            removeLineItem={removeLineItem}
-            updateLineItem={updateLineItem}
-          />
-        ))}
+        <List>
+          {lineItems && Array.from(lineItems).map(([key, lineItem], index) => (
+            <ListItem key={key} disableGutters>
+              <div>
+                <h1>{`Line Item: ${index}`}</h1>
+                <LineItem
+                lineItem={lineItem}
+                lineItemError={lineItemErrors.get(key)}
+                mapID={key}
+                removeLineItem={removeLineItem}
+                updateLineItem={updateLineItem}
+                />
+              </div>
+            </ListItem>
+          ))}
+        </List>
+        {/* {lineItems && Array.from(lineItems).map(([key, lineItem]) => (
+              <LineItem
+              key={key}
+              lineItem={lineItem}
+              lineItemError={lineItemErrors.get(key)}
+              mapID={key}
+              removeLineItem={removeLineItem}
+              updateLineItem={updateLineItem}
+            />
+          ))} */}
       <Grid item xs={12}>
         <IconButton onClick={handleAddLineItem}>
           <AddCircleIcon sx={{ color: 'green' }} />
