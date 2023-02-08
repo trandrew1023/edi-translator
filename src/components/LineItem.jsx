855582a8cfe4659b2dea932c28d322022202f6dd
trandrew1023 <andrewtran1023@live.com>
2023-02-07 20:14:54 -0600

    auto-format

--- src/components/LineItem.jsx
+++ src/components/LineItem.jsx
@@ -1,7 +1,15 @@
+import {
+  InfoOutlined,
+  KeyboardArrowDown,
+  KeyboardArrowRight,
+} from '@mui/icons-material';
 import AddCommentIcon from '@mui/icons-material/AddComment';
 import CommentIcon from '@mui/icons-material/Comment';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 import {
+  Box,
+  Button,
+  Collapse,
   FormControl,
   Grid,
   IconButton,
@@ -12,15 +20,11 @@ import {
   TextField,
   Tooltip,
   Typography,
-  Button,
-  Box,
-  Collapse
 } from '@mui/material';
 import PropTypes from 'prop-types';
 import { React, useState } from 'react';
 import poLineStatusCodes from '../static/data/poLineStatusCodes.json';
 import CommentModal from './CommentModal';
-import { InfoOutlined, KeyboardArrowDown, KeyboardArrowRight } from '@mui/icons-material';
 
 function LineItem({
   index,
@@ -39,12 +43,21 @@ function LineItem({
 
   const handleLineItemCollapse = () => {
     setOpen(!open);
-  }
+  };
 
   return (
     <Box>
-      <Button variant='text' onClick={handleLineItemCollapse} startIcon={open ? <KeyboardArrowDown /> : <KeyboardArrowRight />}>{`Line Item: ${index + 1} (${lineItem.item})`}</Button>
-      <Collapse in={open} sx={{paddingTop: '10px'}} timeout="auto" unmountOnExit>
+      <Button
+        variant="text"
+        onClick={handleLineItemCollapse}
+        startIcon={open ? <KeyboardArrowDown /> : <KeyboardArrowRight />}
+      >{`Line Item: ${index + 1} (${lineItem.item})`}</Button>
+      <Collapse
+        in={open}
+        sx={{ paddingTop: '10px' }}
+        timeout="auto"
+        unmountOnExit
+      >
         <Grid container spacing={2} mb={4}>
           <Grid item md={4}>
             <TextField
@@ -91,7 +104,9 @@ function LineItem({
               error={lineItemError?.acknowledgedQuantity}
               label="Acknowledged quantity"
               value={lineItem.acknowledgedQuantity}
-              onChange={(event) => handleEventChange(event, 'acknowledgedQuantity')}
+              onChange={(event) =>
+                handleEventChange(event, 'acknowledgedQuantity')
+              }
             />
           </Grid>
           <Grid item md={4}>
@@ -109,7 +124,7 @@ function LineItem({
                       <InfoOutlined />
                     </Tooltip>
                   </InputAdornment>
-                )
+                ),
               }}
             />
           </Grid>
