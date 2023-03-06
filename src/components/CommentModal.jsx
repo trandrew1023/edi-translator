08b9c88ad141217de37e043fd9056acd5e478204
Andrew Tran <andrewtran1023@live.com>
2023-03-06 10:41:37 -0600

    update

--- src/components/CommentModal.jsx
+++ src/components/CommentModal.jsx
@@ -2,6 +2,7 @@ import { Box, Button, Grid, Modal, TextField, Typography } from "@mui/material";
 import { React } from "react";
 import PropTypes from "prop-types";
 import { useState } from "react";
+import { modalStyle } from "../common/Styles";
 
 export default function CommentModal({
   commentModalOpen,
@@ -12,6 +13,18 @@ export default function CommentModal({
   target,
 }) {
   const [editComment, setEditComment] = useState(comment);
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "60%",
+    maxWidth: "300px",
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   const handleSubmit = () => {
     setComment({ target: { value: editComment } }, target);
@@ -27,7 +40,7 @@ export default function CommentModal({
 
   return (
     <Modal open={commentModalOpen} onClose={() => setCommentModalOpen(false)}>
-      <Box component="form" sx={modalStyle}>
+      <Box component="form" sx={style}>
         <Grid container>
           <Grid item xs={12}>
             <Typography sx={{ color: "text.primary" }}>{header}</Typography>
@@ -68,10 +81,28 @@ export default function CommentModal({
 }
 
 CommentModal.propTypes = {
+  /**
+   * If `true`, the modal is shown.
+   */
   commentModalOpen: PropTypes.bool.isRequired,
+  /**
+   * Callback function to toggle commentModalOpen.
+   */
   setCommentModalOpen: PropTypes.func.isRequired,
+  /**
+   * The initial comment text.
+   */
   comment: PropTypes.string.isRequired,
+  /**
+   * Callback function to update the comment.
+   */
   setComment: PropTypes.func.isRequired,
+  /**
+   * The modal text header displayed above the text field.
+   */
   header: PropTypes.string.isRequired,
+  /**
+   * TODO
+   */
   target: PropTypes.string.isRequired,
 };
